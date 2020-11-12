d4b0c25938983ff628f4b8c03f6f1275e4047892
Matt Sze <msze400@gmail.com>
2020-11-11 21:42:35 -0500

    almost there

--- server.js
+++ server.js
@@ -1,94 +1,125 @@
 const {
-  syncAndSeed,
-  models: { People, Places, Things, Purchases },
-} = require('./db');
-const express = require('express');
-const app = express();
-const html = require('html-template-tag');
+    syncAndSeed,
+    models: { People, Places, Things, Purchases },
+} = require('./db')
+const express = require('express')
+const app = express()
+const html = require('html-template-tag')
 
-app.use(require('method-override')('_method'));
-app.use(express.urlencoded({ extended: false }));
+app.use(require('method-override')('_method'))
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async (req, res, next) => {
-  try {
-    const [peoplepop, placespop, thingspop] = await Promise.all([
-      People.findAll(),
-      Places.findAll(),
-      Things.findAll(),
-      console.log(People.count()),
-    ]);
-    res.send(html`
-      <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <title>acme test</title>
-          <link href="main.css" rel="stylesheet" />
-        </head>
-        <body>
-          <h1>People, Places, and Things w/Dates!</h1>
-          <div class="box">
-            <div class="left-col">
-              <h2>People (${peoplepop.length})</h2>
-              <ul>
-                ${peoplepop.map((ppl) => html`<li>${ppl.name}</li>`)}
-              </ul>
-              <h2>Places (${placespop.length})</h2>
-              <ul>
-                ${placespop.map((elem) => html`<li>${elem.name}</li>`)}
-              </ul>
-              <h2>Things (${thingspop.length})</h2>
-              <ul>
-                ${thingspop.map((elem) => html`<li>${elem.name}</li>`)}
-              </ul>
-            </div>
-            <div class="right-col">
-              <p>Form below</p>
-              <form method="POST" action="/purchases?_method=PUT">
-                <select name="people">
-                  ${peoplepop.map((ppl) => html`<option>${ppl.name}</option>`)}
-                </select>
-                <select name="place">
-                  ${placespop.map(
-                    (elem) => html`<option>${elem.name}</option>`
-                  )}
-                </select>
-                <select name="thing">
-                  ${thingspop.map(
-                    (elem) => html`<option>${elem.name}</option>`
-                  )}
-                </select>
-                <input type="text" name="count" />
-                <input type="date" name="date" />
-                <button>Create Purchase</button>
-              </form>
-            </div>
-          </div>
-        </body>
-      </html>
-    `);
-  } catch (ex) {
-    next(ex);
-  }
-});
+    try {
+        const [
+            peoplepop,
+            placespop,
+            thingspop,
+            purchasespop,
+        ] = await Promise.all([
+            People.findAll(),
+            Places.findAll(),
+            Things.findAll(),
+            Purchases.findAll(),
+            console.log(People.count()),
+        ])
+        res.send(html`
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1.0"
+                    />
+                    <title>acme test</title>
+                    <link href="main.css" rel="stylesheet" />
+                </head>
+                <body>
+                    <h1>People, Places, and Things w/Dates!</h1>
+                    <div class="box">
+                        <div class="left-col">
+                            <h2>People (${peoplepop.length})</h2>
+                            <ul>
+                                ${peoplepop.map(
+                                    (ppl) => html`<li>${ppl.name}</li>`
+                                )}
+                            </ul>
+                            <h2>Places (${placespop.length})</h2>
+                            <ul>
+                                ${placespop.map(
+                                    (elem) => html`<li>${elem.name}</li>`
+                                )}
+                            </ul>
+                            <h2>Things (${thingspop.length})</h2>
+                            <ul>
+                                ${thingspop.map(
+                                    (elem) => html`<li>${elem.name}</li>`
+                                )}
+                            </ul>
+                        </div>
+                        <div class="right-col">
+                            <p>Form below</p>
+                            <form method="POST" action="/purchases?_method=PUT">
+                                <select name="people">
+                                    ${peoplepop.map(
+                                        (ppl) =>
+                                            html`<option>${ppl.name}</option>`
+                                    )}
+                                </select>
+                                <select name="place">
+                                    ${placespop.map(
+                                        (elem) =>
+                                            html`<option>${elem.name}</option>`
+                                    )}
+                                </select>
+                                <select name="thing">
+                                    ${thingspop.map(
+                                        (elem) =>
+                                            html`<option>${elem.name}</option>`
+                                    )}
+                                </select>
+                                <input type="text" name="count" />
+                                <input type="date" name="date" />
+                                <button>Create Purchase</button>
+                                <div class="purchases">
+                                    <ul>
+                                        ${purchasespop.map(
+                                            (purchase) =>
+                                                html`<li>
+                                                    ${purchase.people} purchased
+                                                    ${purchase.count} in
+                                                    ${purchase.place} on
+                                                    ${purchase.date}
+                                                </li>`
+                                        )}
+                                    </ul>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </body>
+            </html>
+        `)
+    } catch (ex) {
+        next(ex)
+    }
+})
 
-app.put('/', async (res, req, next) => {
-  console.log(req.body);
-});
+app.put('/purchases', async (res, req, next) => {
+    console.log(req.body)
+    res.redirect('/')
+})
 
 const init = async () => {
-  try {
-    await syncAndSeed();
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => console.log(`listening on port ${port}`));
-  } catch (ex) {
-    console.log(ex);
-  }
-  console.log('ready');
-};
+    try {
+        await syncAndSeed()
+        const port = process.env.PORT || 3000
+        app.listen(port, () => console.log(`listening on port ${port}`))
+    } catch (ex) {
+        console.log(ex)
+    }
+    console.log('ready')
+}
 
-init();
+init()
